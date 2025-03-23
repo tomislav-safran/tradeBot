b23a448cd74c8fba51726acfc234d6aef6d5bee7
Tomislav <tsafran222@gmail.com>
2025-03-23 23:38:39 +0100

    use GPT_4o model for automated trades

--- src/main/kotlin/service/OpenAIService.kt
+++ src/main/kotlin/service/OpenAIService.kt
@@ -27,7 +27,7 @@ private val logger = KotlinLogging.logger {}
 object OpenAIService {
     private fun getGPTCompletion(devMessage: String, userMessage: String, responseSchema: JsonSchema.Schema): String? {
         val createParams: ChatCompletionCreateParams = ChatCompletionCreateParams.builder()
-            .model(ChatModel.GPT_4_5_PREVIEW)
+            .model(ChatModel.GPT_4O)
             .maxCompletionTokens(2048)
             .responseFormat(ResponseFormatJsonSchema.builder()
                 .jsonSchema(JsonSchema.builder()
