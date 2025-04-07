6ad335aad7b2ba711007cdab66f25a2948c15a2a
Tomislav <tsafran222@gmail.com>
2025-04-07 22:16:26 +0200

    use gpt O3_MINI

--- src/main/kotlin/service/OpenAIService.kt
+++ src/main/kotlin/service/OpenAIService.kt
@@ -27,7 +27,7 @@ private val logger = KotlinLogging.logger {}
 object OpenAIService {
     private fun getGPTCompletion(devMessage: String, userMessage: String, responseSchema: JsonSchema.Schema): String? {
         val createParams: ChatCompletionCreateParams = ChatCompletionCreateParams.builder()
-            .model(ChatModel.O1)
+            .model(ChatModel.O3_MINI)
             .maxCompletionTokens(2048)
             .responseFormat(ResponseFormatJsonSchema.builder()
                 .jsonSchema(JsonSchema.builder()
